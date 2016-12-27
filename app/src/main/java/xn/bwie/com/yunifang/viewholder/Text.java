a46b49cefa9d8ee1721e0d225c177664f44e5084
XuJiaJian <1274913838@qq.com>
2016-12-27 16:03:41 +0800

    sssssss555

--- app/src/main/java/xn/bwie/com/yunifang/viewholder/Text.java
+++ app/src/main/java/xn/bwie/com/yunifang/viewholder/Text.java
@@ -7,9 +7,9 @@ import android.widget.Toast;
  * Created by asus on 2016/12/27.
  */
 
-public class Text extends Activity{
+public class Text extends Activity {
     //爸爸爱你
-    public void Main(){
+    public void Main() {
         Toast.makeText(Text.this, "....", Toast.LENGTH_SHORT).show();
         //今天是徐嘉健的忌日！
         //今天是徐嘉健的忌日！
@@ -17,4 +17,11 @@ public class Text extends Activity{
         //今天是徐嘉健的忌日！
         //今天是徐嘉健的忌日！
     }
+
+
+        //龙儿
+        //☟☟☟☟☟☟☟☟☟☟☟☟☟
+        // 爸爸爱你
+        //☺
+
 }

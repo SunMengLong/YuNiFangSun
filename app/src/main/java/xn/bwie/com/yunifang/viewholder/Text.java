8ba30176ca076256e06be183824b4e6fd39f2a89
SunMengLong2016 <13121980161@163.com>
2016-12-27 15:55:11 +0800

    我添加了一个方法！

--- app/src/main/java/xn/bwie/com/yunifang/viewholder/Text.java
+++ app/src/main/java/xn/bwie/com/yunifang/viewholder/Text.java
@@ -1,10 +1,16 @@
 package xn.bwie.com.yunifang.viewholder;
 
+import android.app.Activity;
+import android.widget.Toast;
+
 /**
  * Created by asus on 2016/12/27.
  */
 
-public class Text {
+public class Text extends Activity{
     //爸爸爱你
-
+    public void Main(){
+        Toast.makeText(Text.this, "....", Toast.LENGTH_SHORT).show();
+        //今天是徐嘉健的忌日！
+    }
 }
